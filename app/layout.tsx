17424e810bc0ee9eff8f30259b35a8fa283906d5
Trend20 <enockomondi305@gmail.com>
2024-05-14 15:40:45 +0300

    adding homepage components

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
 import Header from "@/components/header/Header";
+import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
           {children}
         </main>
       {/*  footer component*/}
+          <Footer />
       </div>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import Hero from "@/components/hero/page";
+import Features from "@/components/features/page";
+import Pricing from "@/components/pricing/Pricing";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      HomePage
+      <Hero />
+        <Features />
+        <Pricing />
     </main>
   );
 }
