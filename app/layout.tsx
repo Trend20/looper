959e3f788b5254c40e8a8686b4351d4953520e15
Trend20 <enockomondi305@gmail.com>
2024-05-14 15:23:15 +0300

    adding the header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
+import Header from "@/components/header/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
       <div>
         {/*header component*/}
+          <Header />
         <main>
           {children}
         </main>
