1624261e88da4d876b0da81b02c98c16fcb266c5
Trend20 <enockomondi305@gmail.com>
2024-05-17 14:43:21 +0300

    prisma configs

---
+++ app/api/bookmarks/route.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+// creating a bookmark
+export async function POST(request: Request) {
+    try {
+        const body = await request.json();
+        const { title, description, url } = body;
+        const collection = await prisma.collection.create({
+            data: {
+                title,
+                description,
+                url
+            },
+        });
+        return NextResponse.json(collection, { status: 201 });
+    } catch (error) {
+        return NextResponse.json({ message: error }, { status: 500 });
+    }
+}
+
+// getting all bookmarks
+export async function GET() {
+    try {
+        const res = await prisma.collection.findMany();
+        return NextResponse.json(res, { status: 201 });
+    } catch (error) {
+        return NextResponse.json(
+            { message: "Error Occurred while fetching bookmarks" },
+            { status: 500 }
+        );
+    }
+}
