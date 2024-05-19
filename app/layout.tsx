6b58689d3bb372c1aa8a11076cfdb07e0a562959
Trend20 <enockomondi305@gmail.com>
2024-05-19 12:02:34 +0300

    adding a loader

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,41 @@
-import type { Metadata } from "next";
+"use client";
+
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
 import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
 import {NextAuthProvider} from "@/app/providers";
-
+import {useEffect, useState} from "react";
+import Loader from "@/components/common/Loader";
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Looper",
-  description: "Looper App",
-};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
+    const [loading, setLoading] = useState<boolean>(true);
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 1000);
+    }, []);
+
+    return (
     <html lang="en">
       <body className={inter.className}>
       <NextAuthProvider>
-          <div className="min-h-screen">
-              {/*header component*/}
-              <Header/>
-              <main>
-                  {children}
-              </main>
-              {/*  footer component*/}
-              <Footer/>
-          </div>
+          {
+              loading ? <Loader/> : <div className="min-h-screen">
+                  {/*header component*/}
+                  <Header/>
+                  <main>
+                      {children}
+                  </main>
+                  {/*  footer component*/}
+                  <Footer/>
+              </div>
+          }
       </NextAuthProvider>
       </body>
     </html>
-  );
+    );
 }
